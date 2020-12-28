2a8df759d06a0a1989e7d9524bdc64cad7c19a35
Bernard Lucas <blucas1597@gmail.com>
2020-12-28 16:31:22 -0500

    added .env and initialize for api call

--- server/routes/stock.js
+++ server/routes/stock.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var router = express.Router();
-var db = require("../db/models/database");
+const express = require("express");
+const router = express.Router();
+const db = require("../db/models/database");
 
 router.get("/", function (req, res, next) {
   //TODO: handle general query
@@ -8,49 +8,61 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/:symbol", async (req, res, next) => {
-  var symbol = req.params.symbol;
+  const symbol = req.params.symbol;
 
   if (!symbol) {
     //handle undefined params
-    let err = {
+    const err = {
       name: "error",
       constraint: "missing params",
     };
     res.send(err);
   } else {
     try{
-      let stock = await db.stock.findAll({
+      const stock = await db.stock.findAll({
         where:{
           symbol: symbol.toUpperCase()
         }
       });
+      
       console.log('get symbol: ', stock);
       res.send(JSON.stringify(stock, null, 2));
     }catch(err){
       console.error("error getting symbol: ", err);
       res.send("error retrieving: ", err);
+
     }
   }
 });
 
-router.post("/:symbol", function (req, res, next) {
-  var body = req.body;
+router.post("/:symbol", async (req, res, next) => {
+  const symbol = req.params.symbol;
 
-  var newStock = db.stock.build({
-    symbol: body.symbol,
-    company: body.company,
-    sector: body.sector,
-    industry: body.industry,
-  });
+  if (!symbol) {
+    //handle undefined params
+    const err = {
+      name: "error",
+      constraint: "missing params",
+    };
+    res.send(err);
+  } else {
+    console.log('saving'); 
+  }
+  // const newStock = db.stock.build({
+  //   symbol: body.symbol,
+  //   company: body.company,
+  //   sector: body.sector,
+  //   industry: body.industry,
+  // });
 
-  newStock
-    .save()
-    .then((saveres) => {
-      console.log("new stock saved: ", saveres);
-    })
-    .catch((err) => {
-      console.error("error saving new stock: ", err);
-    });
+  // newStock
+  //   .save()
+  //   .then((saveres) => {
+  //     console.log("new stock saved: ", saveres);
+  //   })
+  //   .catch((err) => {
+  //     console.error("error saving new stock: ", err);
+  //   });
 });
 
 module.exports = router;
